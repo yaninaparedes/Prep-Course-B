7151c7c53cb3f7036f22be582f2dc7e8b77c1c0d
yaninaparedes <yany18-88@hotmail.com>
2022-10-17 19:53:24 -0300

    01-JS-I/homework/homework.js

--- 01-JS-I/homework/homework.js
+++ 01-JS-I/homework/homework.js
@@ -4,10 +4,10 @@
 const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 18;
+const nuevoNum = 20;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
